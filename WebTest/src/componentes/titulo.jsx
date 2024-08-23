d48a3f504ade4a765af5343d4993b20337ab22c9
crogger <118250093+crogger@users.noreply.github.com>
2024-08-23 06:21:20 +0000

    he agregado el componente tirulo en la wep primcipal

---
+++ WebTest/src/componentes/titulo.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+function Titulo (){
+    return(
+          <>
+          <h1>Conoce el peru primero</h1>
+          <h2>En el peru existen hermosis lugares</h2>
+          </>
+    );
+}
+export default Titulo;
