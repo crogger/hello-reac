cb6c29ff7ed914bc4cd06439427334ad9eb612d8
crogger <118250093+crogger@users.noreply.github.com>
2024-08-23 06:29:07 +0000

    hemos agregado el componente pie de pagina y hemos borrado los estilos de index.css

--- WebTest/src/App.jsx
+++ WebTest/src/App.jsx
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Titulo from "./componentes/titulo";
+import Pie from './componentes/pie';
 
 function App(){
   return(
      <>
        <Titulo/>
+       <Pie/>
      </>
   );
 }
